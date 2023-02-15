2c794ea7e1dc54f77b5f4ed0085c434edc58afa6
Clara <clarapachecodev@gmail.com>
2023-02-15 19:47:22 -0300

    Import variables from elements.js and Events function from events.js + destructuring elements object and call Events function injecting dependencies

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-const forestCard = document.querySelector('.forest')
-const rainCard = document.querySelector('.rain')
-const cafeteriaCard = document.querySelector('.cafeteria')
-const fireplaceCard = document.querySelector('.fireplace')
+import { elements } from './js/elements.js'
 
-function toggleForestCard(){
-  const forestCardContainer = document.querySelector('#icon-forest') // icon-container
-  const bgForestCard = document.querySelector('#bg-icon-forest')  // icon-background
-  const forestCardPath = document.querySelector('#path-icon-forest') // icon-drawing
+import { Events } from './js/events.js'
 
-  bgForestCard.classList.toggle('activeBg')
-  forestCardPath.classList.toggle('activePath')
-}
+const {
+  forestCard,
+  rainCard,
+  cafeteriaCard,
+  fireplaceCard
+} = elements 
 
-function toggleRainCard(){
-  const bgRainCard = document.querySelector('#bg-icon-rain')  // icon-background
-  const rainCardPath = document.querySelector('#path-icon-rain') // icon-drawing
+Events(
+  {forestCard,
+  rainCard,
+  cafeteriaCard,
+  fireplaceCard})
 
-  bgRainCard.classList.toggle('activeBgRain')
-  rainCardPath.classList.toggle('activePathRain')
-}
-
-function toggleCafeteriaCard() {
-  const bgCafeteriaCard = document.querySelector('#bg-icon-cafeteria')  // icon-background
-  const cafeteriaCardPath = document.querySelector('#path-icon-cafeteria') // icon-drawing
-
-  bgCafeteriaCard.classList.toggle('activeBg')
-  cafeteriaCardPath.classList.toggle('activePath')
-}
-
-function toggleFireplaceCard() {
-  const bgFireplaceCard = document.querySelector('#bg-icon-fireplace')  // icon-background
-  const fireplaceCardPath = document.querySelector('#path-icon-fireplace') // icon-drawing
-
-  bgFireplaceCard.classList.toggle('activeBg')
-  fireplaceCardPath.classList.toggle('activePath')
-}
-
-
-forestCard.addEventListener('click', () => {
-  toggleForestCard()
-})
-
-rainCard.addEventListener('click', () => {
-  toggleRainCard()
-})
-
-cafeteriaCard.addEventListener('click', () => {
-  toggleCafeteriaCard()
-})
-
-fireplaceCard.addEventListener('click', () => {
-  toggleFireplaceCard()
-})
