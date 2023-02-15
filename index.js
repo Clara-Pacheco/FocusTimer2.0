c016e46286b32f069b09b2f6472d8c9886ef6582
Clara <clarapachecodev@gmail.com>
2023-02-15 19:02:54 -0300

    Create click events to each card that call each one a toggle function

--- index.js
+++ index.js
@@ -12,19 +12,43 @@ function toggleForestCard(){
   forestCardPath.classList.toggle('activePath')
 }
 
+function toggleRainCard(){
+  const bgRainCard = document.querySelector('#bg-icon-rain')  // icon-background
+  const rainCardPath = document.querySelector('#path-icon-rain') // icon-drawing
+
+  bgRainCard.classList.toggle('activeBgRain')
+  rainCardPath.classList.toggle('activePathRain')
+}
+
+function toggleCafeteriaCard() {
+  const bgCafeteriaCard = document.querySelector('#bg-icon-cafeteria')  // icon-background
+  const cafeteriaCardPath = document.querySelector('#path-icon-cafeteria') // icon-drawing
+
+  bgCafeteriaCard.classList.toggle('activeBg')
+  cafeteriaCardPath.classList.toggle('activePath')
+}
+
+function toggleFireplaceCard() {
+  const bgFireplaceCard = document.querySelector('#bg-icon-fireplace')  // icon-background
+  const fireplaceCardPath = document.querySelector('#path-icon-fireplace') // icon-drawing
+
+  bgFireplaceCard.classList.toggle('activeBg')
+  fireplaceCardPath.classList.toggle('activePath')
+}
+
 
 forestCard.addEventListener('click', () => {
   toggleForestCard()
 })
 
 rainCard.addEventListener('click', () => {
-  toggleForestCard()
+  toggleRainCard()
 })
 
 cafeteriaCard.addEventListener('click', () => {
-  toggleForestCard()
+  toggleCafeteriaCard()
 })
 
 fireplaceCard.addEventListener('click', () => {
-  toggleForestCard()
+  toggleFireplaceCard()
 })
