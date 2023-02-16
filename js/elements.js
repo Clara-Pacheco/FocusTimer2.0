1cf923235b100d42d505f0109382750dddabfbaf
Clara <clarapachecodev@gmail.com>
2023-02-15 21:13:58 -0300

    Add more variables for elements module and export them to main js file through the object elements

--- js/elements.js
+++ js/elements.js
@@ -2,10 +2,27 @@ const forestCard = document.querySelector('.forest')
 const rainCard = document.querySelector('.rain')
 const cafeteriaCard = document.querySelector('.cafeteria')
 const fireplaceCard = document.querySelector('.fireplace')
+const bgForestCard = document.querySelector('#bg-icon-forest')  // icon-background
+const forestCardPath = document.querySelector('#path-icon-forest') // icon-drawing
+const bgRainCard = document.querySelector('#bg-icon-rain')  // icon-background
+const rainCardPath = document.querySelector('#path-icon-rain') // icon-drawing
+const bgCafeteriaCard = document.querySelector('#bg-icon-cafeteria')  // icon-background
+const cafeteriaCardPath = document.querySelector('#path-icon-cafeteria') // icon-drawing
+const bgFireplaceCard = document.querySelector('#bg-icon-fireplace')  // icon-background
+const fireplaceCardPath = document.querySelector('#path-icon-fireplace') // icon-drawing
 
  export const elements = {
   forestCard,
   rainCard,
   cafeteriaCard,
-  fireplaceCard
+  fireplaceCard,
+  bgForestCard,
+  forestCardPath,
+  bgRainCard,
+  rainCardPath,
+  bgCafeteriaCard,
+  cafeteriaCardPath,
+  bgFireplaceCard,
+  fireplaceCardPath
+
 }
