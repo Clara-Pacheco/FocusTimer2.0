50d37eadf3ab7bf6ec592bc5f756385b5924c815
Clara <clarapachecodev@gmail.com>
2023-02-16 17:06:54 -0300

    Create counter for playBtn click event, clear it with stopBtn click  event + implement logic for + and - button clicks events

--- js/events.js
+++ js/events.js
@@ -40,6 +40,47 @@ export function Events({
     fireplaceCardPath.classList.toggle('activePath')
   }
 
+  let countDownTimer 
+
+  function countDown(){
+   
+      countDownTimer = setTimeout(()=>{
+
+      let minutesDisplay = Number(minutes.textContent)
+      let secondsDisplay = Number(seconds.textContent)
+      let isFinished = minutesDisplay <= 0 && secondsDisplay <= 0
+
+      if(isFinished){
+        Sounds.timeOff()
+        updateTimerDisplay(minutesDisplay,secondsDisplay)
+        Sounds.stopCafeteriaSound()
+        Sounds.stopFireplaceSound()
+        Sounds.stopForestSound()
+        Sounds.stopRainSound()
+        return
+      }
+
+      if(secondsDisplay <= 0){
+        secondsDisplay = 60
+        --minutesDisplay
+      }
+
+      updateTimerDisplay(minutesDisplay, String(secondsDisplay - 1).padStart(2, "0"));
+
+      
+    countDown()
+
+    },1000)
+
+  }
+
+  function updateTimerDisplay(minutesDisplay,secondsDisplay){
+    minutes.innerText = String(minutesDisplay).padStart(2, "0");
+    seconds.innerText = String(secondsDisplay).padStart(2, "0");
+  }
+
+
+  // Card events
 
   forestCard.addEventListener('click', () => {
     toggleForestCard()
@@ -78,6 +119,41 @@ export function Events({
       Sounds.stopFireplaceSound()
     }
    
+  })
+
+  // Timer Events
+
+  btnPlay.addEventListener('click', () => {
+    countDown()
+   
+  })
+
+  btnStop.addEventListener('click', () => {
+    clearTimeout(countDownTimer)
+  })
+
+  btnPlus.addEventListener('click', () => {
+    seconds.innerText = Number(seconds.innerText) + 5
+    if(seconds.innerText >= 60){
+      seconds.innerText = 0
+      minutes.innerText = Number(minutes.innerText) + 1
+    }
+    updateTimerDisplay(minutes.innerText,seconds.innerText)
+    
+  })
+
+  btnMinus.addEventListener('click', () => {
+    seconds.innerText = Number(seconds.innerText) - 5
+    if(seconds.innerText <= 0){
+      seconds.innerText = 0
+      minutes.innerText = Number(minutes.innerText) - 1
+    }
+    if(minutes.innerText < 0){
+      minutes.innerText = 0
+      seconds.innerText = 0
+    }
+
+    updateTimerDisplay(minutes.innerText,seconds.innerText)
   })
 
 }
