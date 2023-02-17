3b31d4eda8275805d3977bccf28bd9256cb1cfad
Clara <clarapachecodev@gmail.com>
2023-02-16 21:02:13 -0300

    Remove unecessary dependencies + remove controls and timer functionalities to their especific modules+ adjustments using timer, Sounds and controls objects + improve +/- buttons' logic

--- js/events.js
+++ js/events.js
@@ -4,13 +4,9 @@ export function Events({
   cafeteriaCard,
   fireplaceCard,
   bgForestCard,
-  forestCardPath,
   bgRainCard,
-  rainCardPath,
   bgCafeteriaCard,
-  cafeteriaCardPath,
   bgFireplaceCard,
-  fireplaceCardPath,
   Sounds,
   minutes,
   seconds,
@@ -18,87 +14,9 @@ export function Events({
   btnStop,
   btnPlus,
   btnMinus,
-  controls
+  controls,
+  timer
 }){
-
-  // function toggleForestCard(){
-  //   bgForestCard.classList.toggle('activeBg')
-  //   forestCardPath.classList.toggle('activePath')
-  // }
-
-  // function toggleRainCard(){
-  //   bgRainCard.classList.toggle('activeBg')
-  //   rainCardPath.classList.toggle('activePath')
-  // }
-
-  // function toggleCafeteriaCard() {
-  //   bgCafeteriaCard.classList.toggle('activeBg')
-  //   cafeteriaCardPath.classList.toggle('activePath')
-  // }
-
-  // function toggleFireplaceCard() {
-  //   bgFireplaceCard.classList.toggle('activeBg')
-  //   fireplaceCardPath.classList.toggle('activePath')
-  // }
-
-  // function endToggleCards(){
-  //   if(bgForestCard.classList.contains('activeBg')){
-  //     toggleForestCard()
-  //   }
-
-  //   if(bgRainCard.classList.contains('activeBg')){
-  //     toggleRainCard()
-  //   }
-
-  //   if(bgCafeteriaCard.classList.contains('activeBg')){
-  //     toggleCafeteriaCard()
-  //   }
-
-  //   if(bgFireplaceCard.classList.contains('activeBg')){
-  //     toggleFireplaceCard()
-  //   }
-  // }
-
-  let countDownTimer 
-
-  function countDown(){
-   
-      countDownTimer = setTimeout(()=>{
-
-      let minutesDisplay = Number(minutes.textContent)
-      let secondsDisplay = Number(seconds.textContent)
-      let isFinished = minutesDisplay <= 0 && secondsDisplay <= 0
-
-      if(isFinished){
-        Sounds.timeOff()
-        updateTimerDisplay(minutesDisplay,secondsDisplay)
-        endToggleCards()
-        Sounds.stopCafeteriaSound()
-        Sounds.stopFireplaceSound()
-        Sounds.stopForestSound()
-        Sounds.stopRainSound()
-        return
-      }
-
-      if(secondsDisplay <= 0){
-        secondsDisplay = 60
-        --minutesDisplay
-      }
-
-      updateTimerDisplay(minutesDisplay, String(secondsDisplay - 1).padStart(2, "0"));
-
-      
-    countDown()
-
-    },1000)
-
-  }
-
-  function updateTimerDisplay(minutesDisplay,secondsDisplay){
-    minutes.innerText = String(minutesDisplay).padStart(2, "0");
-    seconds.innerText = String(secondsDisplay).padStart(2, "0");
-  }
-
 
   // Card events
 
@@ -144,12 +62,12 @@ export function Events({
   // Timer Events
 
   btnPlay.addEventListener('click', () => {
-    countDown()
+    timer.countDown()
    
   })
 
   btnStop.addEventListener('click', () => {
-    clearTimeout(countDownTimer)
+    timer.hold()
     Sounds.stopCafeteriaSound()
     Sounds.stopFireplaceSound()
     Sounds.stopForestSound()
@@ -167,22 +85,25 @@ export function Events({
       seconds.innerText = 0
       minutes.innerText = Number(minutes.innerText) + 1
     }
-    updateTimerDisplay(minutes.innerText,seconds.innerText)
+    timer.updateTimerDisplay(minutes.innerText,seconds.innerText)
     
   })
 
   btnMinus.addEventListener('click', () => {
-    seconds.innerText = Number(seconds.innerText) - 5
-    if(seconds.innerText <= 0){
-      seconds.innerText = 0
-      minutes.innerText = Number(minutes.innerText) - 1
-    }
-    if(minutes.innerText < 0){
-      minutes.innerText = 0
-      seconds.innerText = 0
+
+    if(seconds.innerText > 0){
+      seconds.innerText = Number(seconds.innerText) - 5
+  
     }
 
-    updateTimerDisplay(minutes.innerText,seconds.innerText)
+    if(seconds.innerText == 0){
+      minutes.innerText = Number(minutes.innerText) - 1
+      seconds.innerText = 60
+    }
+
+    timer.updateTimerDisplay(minutes.innerText,seconds.innerText)
   })
 
+
 }
+
